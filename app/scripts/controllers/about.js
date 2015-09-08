33c42f9c58bf4e4d4bceec45a3738d1b7e5b8039
unknown <murali.y@HTGS-LPT-19.Healthtech.co.in>
2015-09-08 13:41:04 +0530

    changes in abbout controller

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,7 +8,7 @@
  * Controller of the xprshealthApp
  */
 angular.module('xprshealthApp')
-  .controller('AboutCtrl', function () {
+  .controller('AboutCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
